664e2f7442750f07db08a2abd102f44011c887cc
logansql <loganchen@hotmail.com>
2018-02-06 15:22:57 -0500

    sample of PHP web site using PDO_SQLSRV driver from MS

---
+++ SQLPHP/web/start_php_web.ps1
@@ -0,0 +1,7 @@
+#How to start the PHP on ISS express
+#
+cd C:\logansql\SQLPRO\SQLPHP\web
+php -S localhost:8080
+
+# IE:   http://localhost:8080/index.php
+# IE:   http://localhost:8080/adventureworks_demo_pdo.php
