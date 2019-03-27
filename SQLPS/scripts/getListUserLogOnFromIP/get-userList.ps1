b91cb5711c8798aaa6559eb769f66e14a797fff3
logansql <loganchen@hotmail.com>
2019-03-27 10:21:05 -0400

    Powershell scripts to query logon users based on IPs

---
+++ SQLPS/scripts/getListUserLogOnFromIP/get-userList.ps1
@@ -0,0 +1,48 @@
+﻿[CmdletBinding()]
+Param (
+  [string]$IPList,
+  [string]$UserList
+)
+
+#
+
+<#
+$ipAddress="196.18.50.151"
+$hostName=[System.Net.Dns]::GetHostByAddress($ipAddress).Hostname
+(Get-WmiObject -Class win32_process -ComputerName $hostName | Where-Object name -Match explorer).getowner().user
+#>
+
+if (!$IPList -or !$UserList )
+{
+'get current logged on UserList from IPList'
+'.\get-userList.ps1 -IPList yourIpListInputFilename -UserList yourUserListOutputFilename'	
+ exit
+}
+else
+{
+'get-userList.ps1 {0}, {1}' -f $IPList, $UserList
+}
+
+$MyInput = $IPList
+$MyOutput = $UserList
+"IPAddress : HostName : UserLoggedOnLocally">$MyOutput
+"---------   --------   -------------------">>$MyOutput
+Get-Content $MyInput | ForEach-Object{
+    $hostname = ([System.Net.Dns]::GetHostByAddress($_)).Hostname
+    if($? -eq $False) {
+      $hostname ="Cannot resolve hostname"
+      }
+    else
+      {
+      $has_explorer = Get-WmiObject -Class win32_process -ComputerName $hostname | Where-Object name -Match explorer
+      if ($has_explorer) {
+        $username = $has_explorer.getowner().user
+        }
+      else
+        {
+        $username ="No one is logged on locally"
+        }
+      }
+    $_ +" : "+ $hostname +" : "+ $username >> $MyOutput
+}
+get-content $MyOutput
--- SQLPS/scripts/getListUserLogOnFromIP/readme.txt
+++ SQLPS/scripts/getListUserLogOnFromIP/readme.txt
@@ -7,3 +7,24 @@ Get the ip's from C:\logan\scripts\ip-addresses.txt
 One by one, get the hostname from DNS, and query the explorer's user name from the host
 Print IP,HOSTNAME,LogonUser
 Write to C:\temp\ListOfUserLogOnFromIP.txt
+
+get-userList.ps1
+----------------
+Usage:
+	.\get-userList.ps1 -IPList yourIpListInputFilename -UserList yourUserListOutputFilename
+
+get-user-from-IPList.ps1
+------------------------
+Usage:
+	.\get-user-from-IPList.ps1 -IPList yourIpListInputFilename -UserList yourUserListOutputFilename
+
+get-user-from-HostList.ps1
+--------------------------
+This script use PsLoggedon.exe 
+Usage:
+	.\get-user-from-HostList.ps1 -HostList yourHostListInputFilename -UserList yourUserListOutputFilename
+
+Reference
+----------
+PsLoggedon.exe  
+https://docs.microsoft.com/en-us/sysinternals/downloads/psloggedon
